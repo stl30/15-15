43b8e4faa9ad1f8d76e637f87de159e2ef2c8449
Stoea L <stl30@users.noreply.github.com>
2019-03-16 16:04:41 +0200

    in terminal rulezi node server.js accesibil dupa pe localhost:8080

--- js/game.js
+++ js/game.js
@@ -2,12 +2,12 @@
 var config = {
     type: Phaser.AUTO,
     width: 800,
-    height: 600,
+    height: 640,
     physics: {
         default: 'arcade',
         arcade: {
             gravity: { y: 300 },
-            debug: false
+            debug: true
         }
     },
     scene: {
@@ -20,6 +20,9 @@ var config = {
 var player;
 var stars;
 var bombs;
+var bombsTimer;
+var time = new Date();
+var bombTime = time.getMilliseconds();
 var platforms;
 var cursors;
 var score = 0;
@@ -31,35 +34,57 @@ var game = new Phaser.Game(config);
 function preload ()
 {
     this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
+    this.load.image('mountain', 'assets/mountains_3200x640.png');
+    // this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.image("tiles", "assets/Tiles_32x32.png");
+    this.load.tilemapTiledJSON("map", "assets/map.json");
 }
 
 function create ()
 {
-    //  A simple background for our game
-    this.add.image(400, 300, 'sky');
+    // var background = this.add.tileSprite(0, 0, 500, 500, "sky");
+    // //  A simple background for our game
+    this.add.image(1600, 320, 'mountain');
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = this.physics.add.staticGroup();
 
-    //  Here we create the ground.
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    // this.physics.world.setBounds(0, 0, 3392, 240);
+    //
+    // //  The platforms group contains the ground and the 2 ledges we can jump on
+    // platforms = this.physics.add.staticGroup();
+    //
+    // //  Here we create the ground.
+    // //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    // platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    const map = this.make.tilemap({key:"map"});
+    // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
+    // Phaser's cache (i.e. the name you used in preload)
+    const tileset = map.addTilesetImage("Tiles_32x32", "tiles");
+
+    // Parameters: layer name (or index) from Tiled, tileset, x, y
+    const groundLayer = map.createStaticLayer("GroundLayer", tileset, 0, 0);
+
+    groundLayer.setCollisionByExclusion([-1]);
+
 
     //  Now let's create some ledges
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
+    // platforms.create(600, 400, 'ground');
+    // platforms.create(50, 250, 'ground');
+    // platforms.create(750, 220, 'ground');
 
     // The player and its settings
     player = this.physics.add.sprite(100, 450, 'dude');
 
+    this.physics.add.collider(player, groundLayer);
+
+
     //  Player physics properties. Give the little guy a slight bounce.
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
+
 
     //  Our player animations, turning, walking left and walking right.
     this.anims.create({
@@ -82,27 +107,44 @@ function create ()
         repeat: -1
     });
 
+    // player 1 will use WASD
+
+    // the returned object will have the same properties as the cursor keys, so it's easier to work with them
+
+    this.keys = this.input.keyboard.addKeys({
+
+        'bomb': Phaser.Input.Keyboard.KeyCodes.B,
+
+        'fight': Phaser.Input.Keyboard.KeyCodes.F
+
+    });
+
     //  Input Events
     cursors = this.input.keyboard.createCursorKeys();
 
     //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
     stars = this.physics.add.group({
         key: 'star',
-        repeat: 11,
+        repeat: 1,
         setXY: { x: 12, y: 0, stepX: 70 }
     });
 
-    stars.children.iterate(function (child) {
-
-        //  Give each star a slightly different bounce
-        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-
-    });
+    // stars.children.iterate(function (child) {
+    //
+    //     //  Give each star a slightly different bounce
+    //     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    //
+    // });
 
     bombs = this.physics.add.group();
+    this.physics.add.collider(bombs, groundLayer);
 
     //  The score
     scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    livesText = this.add.text(16, 48, 'lives: 3', { fontSize: '32px', fill: '#000' });
+    scoreText.setScrollFactor(0);
+    livesText.setScrollFactor(0);
+
 
     //  Collide the player and the stars with the platforms
     this.physics.add.collider(player, platforms);
@@ -112,7 +154,12 @@ function create ()
     //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
     this.physics.add.overlap(player, stars, collectStar, null, this);
 
-    this.physics.add.collider(player, bombs, hitBomb, null, this);
+    // this.physics.add.collider(player, bombs, hitBomb, null, this);
+
+    const camera = this.cameras.main;
+    camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.startFollow(player, true, 0.05, 0.05);
 }
 
 function update ()
@@ -145,6 +192,11 @@ function update ()
     {
         player.setVelocityY(-330);
     }
+
+    if (Phaser.Input.Keyboard.JustDown(this.keys.bomb))
+    {
+        addBomb(player);
+    }
 }
 
 function collectStar (player, star)
@@ -154,6 +206,7 @@ function collectStar (player, star)
     //  Add and update the score
     score += 10;
     scoreText.setText('Score: ' + score);
+    scoreText.setColor('red')
 
     if (stars.countActive(true) === 0)
     {
@@ -173,6 +226,10 @@ function collectStar (player, star)
         bomb.allowGravity = false;
 
     }
+}
+
+function addBomb(player) {
+    bombs.create(player.x, player.y, 'bomb');
 }
 
 function hitBomb (player, bomb)
